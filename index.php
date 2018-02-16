1b96dafbbe149291f27ce9f11226eea116e488b5
edwindeza <ejdeza@hotmail.com>
2018-02-16 00:31:48 -0500

    Página en construcción

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>OpenLab - Enseñar es aprender</title>
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+	<div id="background">
+		<center class="contenido">
+			<img src="logo_white.png" alt="Logo OpenLab">
+			<h1>PÁGINA EN CONSTRUCCIÓN</h1>
+		</center>
+	</div>
+</body>
+</html>
